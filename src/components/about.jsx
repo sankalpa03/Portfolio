b26fa4372b866cba0e7cda639904c00288ae32d2
Sankalpa_Bhandaari <118331958+sankalpa03@users.noreply.github.com>
2025-11-14 18:37:07 +0545

    new

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const About = () => (
   <section id="about">
-    <h2>About Me</h2>
+    <h2>About Me
+  </h2>
     <p>
       I’m a frontend web developer skilled in React, JavaScript, and CSS. I build
       responsive and modern web applications with a focus on clean design and
